8b586cc096c918a279fdb526d527729803e7ae18
João Pedro Nolasco <nolascojoaops@gmail.com>
2024-12-29 12:54:24 -0300

    Use connection pool in db.js for MariaDB

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,20 @@
 import mysql from "mysql"
 
-export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "teste123",
-    database: "crud"
+export const db = mysql.createPool({
+    connectionLimit: 10,  
+    host: process.env.DB_HOST || "mariadb",  
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "teste123",
+    database: process.env.DB_NAME || "crud"
 })
+
+db.getConnection((err, connection) => {
+    if (err) {
+        console.error("Erro ao conectar ao banco de dados:", err.stack)
+        return
+    }
+    console.log("Conectado ao banco de dados com o ID:", connection.threadId)
+    connection.release()  
+})
+
+export default db;
